d710325ef465d6d711c17d53cfe7a1060aaac98d
Xiwen Cheng <x@cinaq.com>
2024-08-17 21:01:52 +0200

    Very basic calling of mendix-cli executable

---
+++ MendixCLICommand.cs
@@ -0,0 +1,95 @@
+﻿using System.Diagnostics;
+using Mendix.StudioPro.ExtensionsAPI.Model.Projects;
+using Mendix.StudioPro.ExtensionsAPI.Model;
+using System.IO;
+
+
+namespace com.cinaq.MendixCLI;
+
+public class MendixCLICommand
+{
+
+    IModel Model;
+    string ExecutablePath;
+
+    public MendixCLICommand(IModel model)
+    {
+        Model = model;
+        ExecutablePath = Path.Combine(Model.Root.DirectoryPath, "mendix-cli-local");
+    }
+
+    public string exportModel()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = ExecutablePath;
+        startInfo.Arguments = "export-model";
+        startInfo.WorkingDirectory = Model.Root.DirectoryPath;
+        startInfo.RedirectStandardOutput = true;
+
+        Process process = new Process();
+        process.StartInfo = startInfo;
+
+        try
+        {
+            process.Start();
+            process.WaitForExit();
+
+            string output = process.StandardOutput.ReadToEnd();
+            Console.WriteLine(output);
+            return output;
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            return string.Empty; // Or handle the error appropriately
+        }
+    }
+
+    public string lintModel()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = ExecutablePath;
+        startInfo.Arguments = "lint -j lint-results.json";
+        startInfo.WorkingDirectory = Model.Root.DirectoryPath;
+        startInfo.RedirectStandardOutput = true;
+
+        Process process = new Process();
+        process.StartInfo = startInfo;
+
+        try
+        {
+            process.Start();
+            process.WaitForExit();
+
+            string output = process.StandardOutput.ReadToEnd();
+            Console.WriteLine(output);
+            return output;
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            return string.Empty; // Or handle the error appropriately
+        }
+    }
+
+    private void ensureMendixCLI()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = "npm";
+        startInfo.Arguments = "install -g mendix-cli";
+        startInfo.RedirectStandardOutput = true;
+
+        Process process = new Process();
+        process.StartInfo = startInfo;
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+
+        Console.WriteLine("XXX");
+        Console.WriteLine(output);
+
+        process.WaitForExit();
+    }
+}
--- MendixCLIMenuExtension.cs
+++ MendixCLIMenuExtension.cs
@@ -8,9 +8,20 @@ namespace com.cinaq.MendixCLI.MendixExtension;
 [method: ImportingConstructor]
 public class MendixCLIMenuExtension(IDockingWindowService dockingWindowService, IMessageBoxService messageBoxService) : MenuExtension
 {
+
     public override IEnumerable<MenuViewModel> GetMenus()
     {
         yield return new MenuViewModel("Say hello", () => messageBoxService.ShowInformation("Hello World!"));
+        yield return new MenuViewModel("Export Model", () => {
+            MendixCLICommand cmd = new MendixCLICommand(CurrentApp);
+            Console.WriteLine(CurrentApp.Root.DirectoryPath);
+            cmd.exportModel();
+        });
+        yield return new MenuViewModel("Lint Model", () => {
+            MendixCLICommand cmd = new MendixCLICommand(CurrentApp);
+            Console.WriteLine(CurrentApp.Root.DirectoryPath);
+            cmd.lintModel();
+        });
         yield return new MenuViewModel("Open My Dockable Pane", () => dockingWindowService.OpenPane(LintingPaneExtension.ID));
     }
 }
